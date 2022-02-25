a73178b4752128b10054474952044df8c44dcedb
Aleksey Belonogov <aleshka59000@gmail.com>
2022-02-25 08:42:45 +0300

    2 scroll + simple menu

---
+++ forTabs.h
@@ -0,0 +1,14 @@
+//
+// Created by alesh on 10.12.2021.
+//
+
+#ifndef BLOKNOT_FORTABS_H
+#define BLOKNOT_FORTABS_H
+
+
+class forTabs {
+
+};
+
+
+#endif //BLOKNOT_FORTABS_H
--- main.cpp
+++ main.cpp
@@ -30,6 +30,10 @@ LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 /*  Make the class name into a global variable  */
 TCHAR szClassName[] = _T("CodeBlocksWindowsApp");
 
+bool check(char x) {
+    return (x != ' ' && x != '\n' && x != '\r');
+}
+
 int WINAPI WinMain(HINSTANCE hThisInstance,
                    HINSTANCE hPrevInstance,
                    LPSTR lpszArgument,
@@ -39,7 +43,9 @@ int WINAPI WinMain(HINSTANCE hThisInstance,
 //LPSTR lpszArgument, ÓÊÀÇÀÒÅËÜ ÍÀ ÍÀ×ÀËÎ ÑÒÐÎÊÈ
 // int nCmdShow)
 
+
 {
+
     HWND hwnd;               /* This is the handle for our window */
     MSG messages;            /* Here messages to the application are saved */
     WNDCLASSEX wndclass;    /* Data structure for the windowclass */
@@ -55,7 +61,8 @@ int WINAPI WinMain(HINSTANCE hThisInstance,
     wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
     wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
     wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wndclass.lpszMenuName = NULL;                 /* No menu */
+//    wndclass.lpszMenuName = MAKEINTRESOURCE(DLG_MAIN);
+//    wndclass.lpszMenuName=
     wndclass.cbClsExtra = 0;                      /* No extra bytes after the window class */
     wndclass.cbWndExtra = 0;                      /* structure or the window instance */
     /* Use Windows's default colour as the background of the window */
@@ -126,6 +133,69 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
     switch (message)                  /* handle the messages */
 
     {
+//        case WM_COMMAND: {
+//            switch (LOWORD(wParam)) {
+//                //case меню ивенты
+//                switch (LOWORD(wParam)) {
+//                    case MENU_FILE_OPEN: {
+//                        OPENFILENAME ofn;       // common dialog box structure
+//                        char filename[255];     // buffer for file name
+//
+//                        // Initialize OPENFILENAME
+//                        ZeroMemory(&ofn, sizeof(ofn));
+//                        ofn.lStructSize = sizeof(ofn);
+//                        ofn.hwndOwner = hwnd;
+//                        ofn.lpstrFile = filename;
+//                        ofn.lpstrFile[0] = '\0';
+//                        ofn.nMaxFile = sizeof(filename);
+//                        ofn.lpstrFilter = "*.TXT\0";
+//                        ofn.nFilterIndex = 1;
+//                        ofn.lpstrFileTitle = NULL;
+//                        ofn.nMaxFileTitle = 0;
+//                        ofn.lpstrInitialDir = NULL;
+//                        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+//
+//                        if (GetOpenFileName(&ofn) == TRUE) {
+//                            printf("\nfile opened\n");                  //debug info
+//
+//                            ClearFileBuffer(&global->fileBuffer);
+//                            ClearScreenBuffer(&global->screenBuffer);
+//
+//                            global->fileLoaded = ReadToFileBuffer(ofn.lpstrFile, &global->fileBuffer);
+//                            if (global->fileLoaded == TRUE)
+//                                ResizeScreenBuffer(hwnd, &global->screenBuffer, &global->fileBuffer, &global->fontInfo);
+//                        }
+//                    }
+//                        break;
+//                    case MENU_FILE_CLOSE: {
+//                        ClearFileBuffer(&global->fileBuffer);
+//                        ClearScreenBuffer(&global->screenBuffer);
+//                        global->fileLoaded = FALSE;
+//                        InvalidateRect(hwnd, NULL, TRUE);
+//                        ShowScrollBar(hwnd, SB_HORZ, FALSE);
+//                        ShowScrollBar(hwnd, SB_VERT, FALSE);
+//                    }
+//                        break;
+//                    case MENU_FILE_EXIT:
+//                        SendMessage(hwnd, WM_CLOSE, 0, 0);
+//                        break;
+//                    case MENU_CHANGE_MODE: {
+//                        int mode = GetMenuState(GetMenu(hwnd), MENU_CHANGE_MODE, MF_BYCOMMAND);
+//                        if (mode & MF_CHECKED) {
+//                            CheckMenuItem(GetMenu(hwnd), MENU_CHANGE_MODE, MF_BYCOMMAND | MF_UNCHECKED);
+//                            global->screenBuffer.hasHScroll = TRUE;
+//                        } else {
+//                            CheckMenuItem(GetMenu(hwnd), MENU_CHANGE_MODE, MF_BYCOMMAND | MF_CHECKED);
+//                            global->screenBuffer.hasHScroll = FALSE;
+//                        }
+//                        ResizeScreenBuffer(hwnd, &global->screenBuffer, &global->fileBuffer, &global->fontInfo);
+//                    }
+//                        break;
+//                }
+//            }
+//            break;
+//        }
+
         case WM_SIZE:
             cxClient = LOWORD(lParam);
             cyClient = HIWORD(lParam);
@@ -160,15 +230,15 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
             myData.text[fileSize] = '\n';
             int prevI = 0;
             myData.Size = fileSize;
-            for (int i = 0; i <= myData.Size; i++) {
+            for (int i = 0; i < myData.Size; i++) {
                 if (myData.text[i] == '\n') {
                     myData.lenStrings[myData.countStrings] = i - prevI;
                     myData.maxStrLen = max(myData.maxStrLen, myData.lenStrings[myData.countStrings]);
                     if (myData.countStrings != 0) {
                         myData.curSize[myData.countStrings] =
-                                myData.curSize[myData.countStrings - 1] + myData.lenStrings[myData.countStrings];
+                                myData.curSize[myData.countStrings - 1] + myData.lenStrings[myData.countStrings - 1];
                     } else
-                        myData.curSize[myData.countStrings] = myData.lenStrings[0];
+                        myData.curSize[myData.countStrings] = 0;
 
                     myData.countStrings++;
                     prevI = i;
@@ -190,33 +260,58 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
             BeginPaint(hwnd, &ps);
             iPaintBeg = max(long(0), iVscrollPos + ps.rcPaint.top / cyChar - 1);
             iPaintEnd = min(myData.countStrings, iVscrollPos + cyClient);
-            char *buffer = (char *) malloc(sizeof(char) * myData.maxStrLen);
-//            int num = 0;
-            int count = 0;
-            for (int i = iPaintBeg; i < iPaintEnd; i++) {
-                x = cxChar * (1 - iHscrollPos);
-                y = cyChar * (1 - iVscrollPos + i);
-//                auto sysmetrics= GetSystemMetrics();
-                int num = 0;
-                for (int j = 0; j < myData.lenStrings[i]; j++) {
-                    if (j >= iHscrollPos) {
-                        buffer[num] = myData.text[myData.curSize[i] + j];
 
+
+            if (1) {
+                char *buffer = (char *) malloc(sizeof(char) * myData.maxStrLen);
+                for (int i = iPaintBeg; i < iPaintEnd; i++) {
+
+                    y = cyChar * (1 - iVscrollPos + i);
+//                auto sysmetrics= GetSystemMetrics();
+                    int num = 0;
+                    for (int j = iHscrollPos; j < myData.lenStrings[i]; j++) {
+                        buffer[num] = myData.text[myData.curSize[i] + j];
                         num++;
                     }
+                    cerr << "buffer:    " << buffer;
+                    TextOut(hdc, 0, y, buffer, num);
                 }
-                count += myData.lenStrings[i];
+                delete (buffer);
+            } else {
+                char *buffer = (char *) malloc(sizeof(char) * cxClient);
+                int pos = myData.curSize[iPaintBeg];
+                for (int i = iPaintBeg; i < iPaintEnd; i++) {
+                    y = cyChar * (1 - iVscrollPos + i);
+//
+                    int num = 0;
+                    while (num <= cxClient / cxChar && pos + num <= myData.Size) {
+                        buffer[num] = myData.text[pos + num];
+                        num++;
+                    }
+                    pos += num;
+                    if (pos + num <= myData.Size && check(myData.text[pos]) &&
+                        check(myData.text[pos + 1] && check(myData.text[pos - 1]))) {
+                        buffer[num] = '-';
+                        num++;
+                    }
+
+                    TextOut(hdc, 0, y, buffer, num);
+
+                }
+                delete (buffer);
+
+            }
 //                cerr<<count<<"  "<<myData.curSize[i]<<"  "<<myData.lenStrings[i]<<endl;
 //                cerr<<x<<"  "<<y<<"  "<<iPaintBeg<<" "<<i<<" "<<(i-iPaintBeg)*cyChar <<endl;
-                cerr<<buffer;
+//                cerr << buffer;
 
 //                SetTextAlign(hdc, TA_RIGHT | TA_TOP);
-                TextOut(hdc, 0, y, buffer, num);
+
 //                TextOut( hdc, 10, y, buffer,myData.lenStrings[i]);
 //                SetTextAlign(hdc, TA_LEFT | TA_TOP);
 //                66
-            }
-            cerr << x << "  " << y << "  " << iPaintBeg << endl<< endl<< endl<< endl;
+
+//            cerr << x << "  " << y << "  " << iPaintBeg << endl<< endl<< endl<< endl;
             EndPaint(hwnd, &ps);
             return 0;
         }
@@ -297,41 +392,10 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
             break;
         default:
             return DefWindowProc(hwnd, message, wParam, lParam);
-            return 0;
-            /*
-            case WM_VSCROLL :
-                switch (LOWORD(wParam)) {
-                    case SB_LINEUP :
-                        iVscrollPos = std::max(0, iVscrollPos - 1);
-                        break;
-                    case SB_LINEDOWN :
-                        iVscrollPos = std::max(0, iVscrollPos + 1);
-                        break;
-                    case SB_PAGEUP :
-                        iVscrollPos = std::max(0, iVscrollPos - cyClient / cyChar);
-                        break;
-                    case SB_PAGEDOWN :
-                        iVscrollPos = std::max(0, iVscrollPos + cyClient / cyChar);
-                        break;
-                    case SB_THUMBPOSITION :
-                        iVscrollPos = HIWORD(wParam);
-                        break;
-                    default :
-                        break;
-                }
-    //            iVscrollPos = max(0, min(iVscrollPos, NUMLINES));
-                if (iVscrollPos != GetScrollPos(hwnd, SB_VERT)) {
-                    SetScrollPos(hwnd, SB_VERT, iVscrollPos, TRUE);
-                    InvalidateRect(hwnd, NULL, TRUE);
-                }
-                return 0;
-            default:
-                return DefWindowProc(hwnd, message, wParam, lParam);
 
-    */
+
     }
 
 
     return 0;
 }
-
